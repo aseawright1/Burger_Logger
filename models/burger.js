e6f94c91780e2c7dc1f4d60bfbc818d3b317a8f3
Alek <56727628+aseawright1@users.noreply.github.com>
2020-04-05 13:12:26 -0500

    Create burger.js

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+const orm = require('../config/orm')
+
+const burger = {
+    // select all burgers
+    selectAll(cb) {
+        orm.selectAll('burgers', (res) => {
+            cb(res)
+        })
+    },
+    // create new burger
+    insertOne(cols, vals, cb) {
+        orm.insertOne('burgers', cols, vals, (res) => {
+            cb(res)
+        })
+    },
+    // update burger
+    updateOne(objColVals, condition, cb) {
+        orm.updateOne('burgers', objColVals, condi, (res) => {
+            cb(res)
+        })
+    }
+};
+
+module.exports = burger
