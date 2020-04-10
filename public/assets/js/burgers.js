13f18e1b1ddb16d24ee27a043d269317ea818e8e
Alek <56727628+aseawright1@users.noreply.github.com>
2020-04-10 13:43:27 -0500

    add PUT route, but HTML not updating..

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -3,12 +3,28 @@ $(function() {
         event.preventDefault()
         const newBurger = { burger_name: $('#burger').val().trim(), devoured: false }
 
+        //POST
         $.ajax('/api/burgers', {
             type: 'POST',
             data: newBurger
         }).then(function() {
             console.log('New burger added')
             location.reload()
+        })
+    })
+
+    $('.change-burger').on('submit', function(event) {
+        const id = $(this).data('id')
+        const devouredBurger = $(this).data('newDevoured')
+        const newDevCond = { devoured: devouredBurger}
+
+        //PUT
+        $.ajax('/api/burgers/' + id, {
+            type: 'PUT',
+            data: newDevCond
+        }).then(function() {
+            console.log('Devoured is now ', devouredBurger)
+            location.reload
         })
     })
 })
